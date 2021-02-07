1ec254ebae4461ed4313a7445c5b4b37d91a264f
abbaasi69 <46427915+abbaasi69@users.noreply.github.com>
2021-02-07 14:03:14 +0330

    Add files via upload

--- utilities/loadDataset.py
+++ utilities/loadDataset.py
@@ -5,105 +5,43 @@ import pandas as pd
 
 
 def loadDataset(dsname):
-    
-    if dsname == 'MCYT75':
-        img_size = 64
-        class_no = 75
-        img_per_class = 15
-        img_per_class_train = 11
-        img_per_class_test = 4
-        total_img = 1125
-        if True:
-            Train = np.zeros(shape=(class_no * img_per_class_train, img_size, img_size))
-            Test = np.zeros(shape=(class_no * img_per_class_test, img_size, img_size))
-            Train_label = np.zeros(shape=(class_no * img_per_class_train))
-            Test_label = np.zeros(shape=(class_no * img_per_class_test))
-            Tr_cnt = 0
-            Ts_cnt = 0
-            for i in range(total_img):
-                path = './dataset/MCYT/MCYT75/'
-                path = path + str(i // img_per_class + 1)
-                path += 'v'
-                path += str(i % img_per_class) + '.bmp'
 
-                I = plt.imread(path)
-                I = np.reshape(I, (1, img_size, img_size))
-                if (i % img_per_class) < img_per_class_train:
-                    Train[Tr_cnt, :, :] = I
-                    Train_label[Tr_cnt] = i // img_per_class
-                    Tr_cnt = Tr_cnt + 1
-                else:
-                    Test[Ts_cnt, :, :] = I
-                    Test_label[Ts_cnt] = i // img_per_class
-                    Ts_cnt = Ts_cnt + 1
-
-                if i % 100 == 1:
-                    print('image ' + str(i) + " from "+str(total_img))
-
-            Train = (255.0 - Train) / 255.0
-            Test = (255.0 - Test) / 255.0
-
-            # print(Train.max())
-            # print(Train.min())
-            # print(Train.mean())
-
-            Train[Train > 0.15] = 1
-            Train[Train <= 0.15] = 0
-            Test[Test > 0.15] = 1
-            Test[Test <= 0.15] = 0
-            with open('./dataset/MCYT/DS_11_4', 'wb') as f:  # 11 train and 4 test
-                pickle.dump([Train, Train_label, Test, Test_label], f)
-
-        else:
-            with open('./dataset/MCYT/DS_11_4', 'rb') as f:
-                Train, Train_label, Test, Test_label = pickle.load(f)
-            print('Dataset loaded from file')
-
-    if dsname == 'cedar':
+    if dsname == 'Cedar':
         image_size = 64
-        if True:
-            from PIL import Image
-            Train = np.zeros(shape=(880, image_size, image_size))
-            for i in range(880):
-                path = './dataset/cedar/cedar_train/'
-                path = path + str(i + 1) + '.png'
-                # I = plt.imread(path)
-                I = Image.open(path)
-                I.thumbnail((image_size, image_size), Image.ANTIALIAS)
-                I = np.reshape(I, (1, image_size, image_size))
-                Train[i, :, :] = I
-                if i % 100 == 1:
-                    print('train image' + str(i) + "from 880")
+        from PIL import Image
+        Train = np.zeros(shape=(880, image_size, image_size))
+        for i in range(880):
+            path = './dataset/cedar/cedar_train/'
+            path = path + str(i + 1) + '.png'
+            # I = plt.imread(path)
+            I = Image.open(path)
+            I.thumbnail((image_size, image_size), Image.ANTIALIAS)
+            I = np.reshape(I, (1, image_size, image_size))
+            Train[i, :, :] = I
+            if i % 100 == 1:
+                print('train image' + str(i) + "from 880")
 
-            Test = np.zeros(shape=(440, image_size, image_size))
-            for i in range(440):
-                path = './dataset/cedar/cedar_test/'
-                path = path + str(i + 1) + '.png'
-                I = plt.imread(path)
-                I = Image.open(path)
-                I.thumbnail((image_size, image_size), Image.ANTIALIAS)
-                I = np.reshape(I, (1, image_size, image_size))
-                Test[i, :, :] = I
-                if i % 100 == 1:
-                    print('test image' + str(i) + "from 440")
+        Test = np.zeros(shape=(440, image_size, image_size))
+        for i in range(440):
+            path = './dataset/cedar/cedar_test/'
+            path = path + str(i + 1) + '.png'
+            I = plt.imread(path)
+            I = Image.open(path)
+            I.thumbnail((image_size, image_size), Image.ANTIALIAS)
+            I = np.reshape(I, (1, image_size, image_size))
+            Test[i, :, :] = I
+            if i % 100 == 1:
+                print('test image' + str(i) + "from 440")
 
-            Train = (255.0 - Train) / 255.0
-            Test = (255.0 - Test) / 255.0
+        Train = (255.0 - Train) / 255.0
+        Test = (255.0 - Test) / 255.0
 
-            print(Train.max())
-            print(Train.min())
-            print(Train.mean())
 
-            Train[Train > 0.2] = 1
-            Train[Train <= 0.2] = 0
-            Test[Test > 0.2] = 1
-            Test[Test <= 0.2] = 0
+        Train[Train > 0.2] = 1
+        Train[Train <= 0.2] = 0
+        Test[Test > 0.2] = 1
+        Test[Test <= 0.2] = 0
 
-            with open('./dataset/cedar/DS_880_440', 'wb') as f:
-                pickle.dump([Train, Test], f)
-        else:
-            with open('./dataset/cedar/DS_880_440', 'rb') as f:
-                Train, Test = pickle.load(f)
 
         Train_label = pd.read_csv('./dataset/cedar/cedarTrain_label.csv', ',',
                                   header=None).values
@@ -113,4 +51,48 @@ def loadDataset(dsname):
                                  header=None).values
         Test_label = Test_label[:, 0] - 1
 
-    
+        
+    if dsname == 'MCYT75':
+        img_size = 64
+        class_no = 75
+        img_per_class = 15
+        img_per_class_train = 11
+        img_per_class_test = 4
+        total_img = 1125
+        Train = np.zeros(shape=(class_no * img_per_class_train, img_size, img_size))
+        Test = np.zeros(shape=(class_no * img_per_class_test, img_size, img_size))
+        Train_label = np.zeros(shape=(class_no * img_per_class_train))
+        Test_label = np.zeros(shape=(class_no * img_per_class_test))
+        Tr_cnt = 0
+        Ts_cnt = 0
+        for i in range(total_img):
+            path = './dataset/MCYT/MCYT75/'
+            path = path + str(i // img_per_class + 1)
+            path += 'v'
+            path += str(i % img_per_class) + '.bmp'
+
+            I = plt.imread(path)
+            I = np.reshape(I, (1, img_size, img_size))
+            if (i % img_per_class) < img_per_class_train:
+                Train[Tr_cnt, :, :] = I
+                Train_label[Tr_cnt] = i // img_per_class
+                Tr_cnt = Tr_cnt + 1
+            else:
+                Test[Ts_cnt, :, :] = I
+                Test_label[Ts_cnt] = i // img_per_class
+                Ts_cnt = Ts_cnt + 1
+
+            if i % 100 == 1:
+                print('image ' + str(i) + " from "+str(total_img))
+
+        Train = (255.0 - Train) / 255.0
+        Test = (255.0 - Test) / 255.0
+
+
+        Train[Train > 0.15] = 1
+        Train[Train <= 0.15] = 0
+        Test[Test > 0.15] = 1
+        Test[Test <= 0.15] = 0
+        
+
+        
--- utilities/params.py
+++ utilities/params.py
@@ -27,6 +27,16 @@ def getParamCaps(dsname):
     alpha, n_epochs, m_plus, m_minus, lambda_, \
     init_sigma, caps1_n_dims, caps2_n_dims, caps1_n_maps, \
     primary_cap_size1, primary_cap_size2,n_hidden1,n_hidden2 = getParamGeneral()
+    
+    if dsname == 'Cedar':
+        num_class = 55
+        image_size1 = 64
+        image_size2 = 64
+        num_image_channel = 1
+        checkpoint_path = './checkpoints/CEDAR_CapsNet/'
+        primary_cap_size1 = 12
+        primary_cap_size2 = 12
+        lambda_ = 0.02
 
     if dsname == 'MCYT75':
         num_class = 75
@@ -37,17 +47,6 @@ def getParamCaps(dsname):
         primary_cap_size1 = 12
         primary_cap_size2 = 12
         lambda_ = 0.05
-
-    if dsname == 'cedar':
-        num_class = 55
-        image_size1 = 64
-        image_size2 = 64
-        num_image_channel = 1
-        checkpoint_path = './checkpoints/CEDAR_CapsNet/'
-        primary_cap_size1 = 12
-        primary_cap_size2 = 12
-        lambda_ = 0.02
-    
 
     return num_class, image_size1, image_size2, num_image_channel, \
            checkpoint_path, alpha, n_epochs, m_plus, m_minus, lambda_, \
